932f4c2e400b6e6e8c2bebb391608af362db912e
Asif M <asifm.cs2023@citchennai.net>
2025-07-17 14:14:12 +0000

    help to create the human verficatrion part and it should genrate image a

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -6,4 +6,3 @@ import '@/ai/flows/summarize-anomaly-scores.ts';
 import '@/ai/flows/verify-biometrics-flow.ts';
 import '@/ai/flows/send-email-notification-flow.ts';
 import '@/ai/flows/generate-captcha-flow.ts';
-
